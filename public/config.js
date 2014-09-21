5dfd2493b5614ed9cd272acf7ce181684da90576
mikol <mikol.faro@gmail.com>
2014-09-21 18:56:48 +0200

    Added dependency to 'ngLodash'

--- public/config.js
+++ public/config.js
@@ -4,7 +4,7 @@
 var ApplicationConfiguration = (function() {
 	// Init module configuration options
 	var applicationModuleName = 'mean';
-	var applicationModuleVendorDependencies = ['ngResource', 'ui.router', 'ui.bootstrap', 'ui.utils'];
+	var applicationModuleVendorDependencies = ['ngResource', 'ui.router', 'ui.bootstrap', 'ui.utils', 'ngLodash'];
 
 	// Add a new vertical module
 	var registerModule = function(moduleName, dependencies) {
