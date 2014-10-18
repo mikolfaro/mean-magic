a7ef2fa1e937fe49f83800d059eb8c0e3a16d338
mikol <mikol.faro@gmail.com>
2014-10-18 11:14:44 +0200

    Refactoring import, expansions are imported correctly

--- app/tests/expansion.server.controller.test.js
+++ app/tests/expansion.server.controller.test.js
@@ -4,31 +4,58 @@
  * Module dependencies.
  */
 var should = require('should'),
+    nock = require('nock'),
     request = require('supertest'),
     mongoose = require('mongoose'),
-    express = require('express'),
     app = require('../../server'),
     User = mongoose.model('User'),
     Expansion = mongoose.model('Expansion');
 
 /**
- * Globals
- */
-var url = 'http://localhost:3000';
-
-/**
  * Unit tests
  */
 describe('Expansion Controller Unit Tests:', function() {
+    beforeEach(function (done) {
+        nock('https://sites.google.com')
+            .get('/site/mtgfamiliar/manifests/patches.json')
+            .reply(200, {
+                "Patches": [
+                    {
+                        "Name": "An Expansion",
+                        "URL": "https:\/\/sites.google.com\/site\/mtgfamiliar\/patches\/AXP.json.gzip",
+                        "Code": "AXP"
+                    },
+                    {
+                        "Name": "An Other Expansion",
+                        "URL": "https:\/\/sites.google.com\/site\/mtgfamiliar\/patches\/OXP.json.gzip",
+                        "Code": "OXP"
+                    }
+                ]
+            });
+
+        done();
+    });
+
     it ('should be able to import expansions from MTGsalvation patches', function (done) {
         request(app)
             .post('/expansions/import')
             .expect('Content-Type', 'application/json; charset=utf-8')
             .expect(200)
-            .end(function (err, res) {
-                console.log('Ended call');
+            .end(function (err) {
                 should.not.exist(err);
-                done();
+
+                Expansion.findOne({ code: 'AXP' }, function (err, expansion) {
+                    should.not.exist(err);
+                    should.exist(expansion);
+                    expansion.should.have.property('code', 'AXP');
+
+                    Expansion.findOne({ code: 'OXP'}, function (err, expansion2) {
+                        should.not.exist(err);
+                        should.exist(expansion2);
+                        expansion2.should.have.property('code', 'OXP');
+                        done();
+                    });
+                });
             });
     });
 
