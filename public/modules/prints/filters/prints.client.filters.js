8f4fd7f17fcb87188af8fe4ad325bd36b7c30f12
mikol <mikol.faro@gmail.com>
2014-11-29 12:15:26 +0100

    Added expansion symbol with rarity

--- public/modules/prints/filters/prints.client.filters.js
+++ public/modules/prints/filters/prints.client.filters.js
@@ -4,11 +4,26 @@
 angular.module('prints')
 
     .filter('iconifyExpansion', [function () {
-        return function (expansionCode) {
+        return function (expansionCode, rarity) {
             if (!expansionCode || expansionCode.length === 0) {
                 return '';
             }
 
-            return '<img class="expansion-symbol" src="modules/expansions/img/symbol/' + expansionCode + '_symbol.png" alt="' + expansionCode + '" />';
+            rarity = rarity || 'Common';
+            var rarityCode = function (rarity) {
+                if (rarity.indexOf('Timeshifted') > -1) {
+                    return 't';
+                } else if (rarity.indexOf('Mythic') > -1) {
+                    return 'm';
+                } else if (rarity.indexOf('Rare') > -1) {
+                    return 'r';
+                } else if (rarity.indexOf('Uncommon') > -1) {
+                    return 'u';
+                } else {
+                    return 'c';
+                }
+            };
+
+            return '<img class="expansion-symbol" src="http://mtgimage.com/symbol/set/' + expansionCode + '/' + rarityCode(rarity) + '/256.png" />';
         };
     }]);
